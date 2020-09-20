14ec9aeea73f8da08f65d480a66cd7b108ff7af4
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-20 01:07:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
       while element_index < src[row_index].count do
             
             if src[row_index][element_index].to_s == src[row_index][element_index]
-              nested_strings << src[row_index][element_index]
+              nested_strings << src[row_index][element_index].join(" ")
            
              end
              element_index += 1 
